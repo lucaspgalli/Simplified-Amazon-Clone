a92296d814983dd9fbf88ecbc78b9fd201270f17
Lucas <lucaspominigalli@gmail.com>
2024-09-16 01:31:13 -0300

    "Fixed mismatched bracket in event listener callback"

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,5 +73,5 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     const productId = button.dataset.productId;
     addToCart(productId);
     updateCartQuantity();
-  })
+  });
 });
