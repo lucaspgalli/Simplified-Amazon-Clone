05927636046806d43019db54ea1ab28c7ed269ec
Lucas <lucaspominigalli@gmail.com>
2024-09-16 14:43:26 -0300

    "Refactor checkout HTML and JavaScript files, adding new functions and variables for payment summary and product data"

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -18,10 +18,7 @@ export function renderOrderSummary() {
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
     const dateString = deliveryDate.format('dddd, MMMM D');
 
     cartSummaryHTML += `
