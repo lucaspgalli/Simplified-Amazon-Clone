dd8542662a13af791f8d2200883311fd080e80d8
Lucas <lucaspominigalli@gmail.com>
2024-09-18 17:06:25 -0300

    "Added error handling to loadProducts and loadPage functions"

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,14 +6,17 @@ import { loadCart } from '../data/cart.js';
 // import '../data/backend-pratice.js';
 
 async function loadPage() {
-  await loadProductsFetch();
+  try {
+    await loadProductsFetch();
 
-  await new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
+    await new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
     });
-  });
-
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
   renderOrderSummary();
   renderPaymentSummary();
 }
